e9ea60e941e50005c88ec6873a673a2c5ebc6299
Kirk Silva <kirk.ki@hotmail.com>
2024-01-03 17:13:31 -0300

    Adicionando template inicial.

---
+++ src/br/com/kirk/cm/App.java
@@ -0,0 +1,8 @@
+package br.com.kirk.cm;
+
+public class App{
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ test/br/com/kirk/cm/modelo/AppTeste.java
@@ -0,0 +1,16 @@
+package br.com.kirk.cm.modelo;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTeste{
+	
+	@Test
+	void test() {
+		int a = 1+1;
+		
+		assertEquals(2, a);
+	}
+}
