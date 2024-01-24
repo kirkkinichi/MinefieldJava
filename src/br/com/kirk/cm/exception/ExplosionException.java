3b49721662adca181a2bb86ddf4c3535345c8c63
Kirk Silva <kirk.ki@hotmail.com>
2024-01-24 12:42:41 -0300

    Excluindo Exceptions.

--- src/br/com/kirk/cm/exception/ExplosionException.java
+++
@@ -1,5 +0,0 @@
-package br.com.kirk.cm.exception;
-
-public class ExplosionException extends RuntimeException{
-    private static final long serialVersionUID = 1;
-}
--- src/br/com/kirk/cm/exception/SairException.java
+++
@@ -1,5 +0,0 @@
-package br.com.kirk.cm.exception;
-
-public class SairException extends RuntimeException {
-    private static final long serialVersionUID = 1;
-}
